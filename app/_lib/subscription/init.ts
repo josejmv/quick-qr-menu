17536ace9e1eb13e225f08862c20e54299bbdbe5
josejmv <josejmvasquez@gmail.com>
2025-02-11 00:57:03 -0400

    Changes to be committed: 	modified:   app/_lib/subscription/init.ts 	modified:   next.config.ts

--- app/_lib/subscription/init.ts
+++ app/_lib/subscription/init.ts
@@ -4,7 +4,7 @@ import PusherServer from 'pusher'
 export const pusherServer = new PusherServer({
   useTLS: true,
   cluster: 'us2',
-  appId: process.env.NEXT_PUBLIC_SUBSCRIPTION_APP_ID as string,
+  appId: process.env.SUBSCRIPTION_APP_ID as string,
+  secret: process.env.SUBSCRIPTION_SECRET_KEY as string,
   key: process.env.NEXT_PUBLIC_SUBSCRIPTION_PUBLIC_KEY as string,
-  secret: process.env.NEXT_PUBLIC_SUBSCRIPTION_SECRET_KEY as string,
 })
--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,17 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['mongoose'],
-  // experimental: { esmExternals: 'loose' },
   env: {
     AUTH_SECRET: process.env.AUTH_SECRET,
     MONGODB_URI: process.env.MONGODB_URI,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY,
+    SUBSCRIPTION_APP_ID: process.env.SUBSCRIPTION_APP_ID,
+    SUBSCRIPTION_SECRET_KEY: process.env.SUBSCRIPTION_SECRET_KEY,
     NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
     NEXT_PUBLIC_SUBSCRIPTION_PUBLIC_KEY:
       process.env.NEXT_PUBLIC_SUBSCRIPTION_PUBLIC_KEY,
   },
-
-  // webpack: (config) => {
-  //   config.experiments = { topLevelAwait: true }
-  //   return config
-  // },
   async headers() {
     return [
       {
